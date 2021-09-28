8736ad7bbcdc34f0683ac86d2139c093bcc54c47
conda <khaledaboshiesha@icloud.com>
2021-09-28 19:49:54 +0200

    redux + extractor

--- extractor.py
+++ extractor.py
@@ -14,71 +14,63 @@ background_label = tkinter.Label(root, image=background_image)
 background_label.place(relwidth=1,relheight=1)
 
 # functions for the buttons in the software
-class Extract:
-    def readfromimage():
-        path = PathTextBox.get('1.0', 'end-1c')
-        if path:
-            im = Image.open(path)
-            text = pytesseract.image_to_string(im, lang='eng')
-            ResultTextBox.delete('1.0', END)
-            ResultTextBox.insert(END, text)
 
-            def write():
-                # write output to txt file
-                file = open('output.txt', 'w')
-                file.write(ResultTextBox.get('1.0', 'end-1c'))
-                file.close()
-
-            write()
-        else:
-            ResultTextBox.delete('1.0', END)
-            ResultTextBox.insert(END, "FILE CANNOT BE READ")
-
-    def OpenFile():
-        name = askopenfilename(initialdir="/",
-                               filetypes=(("PNG File", "*.png"), ("BMP File", "*.bmp"), ("JPEG File", "*.jpeg"),
-                                          ('PDF File', "*.pdf"), ('DOCX File', '*.docx')),
-                               title="Choose a file."
-                               )
-        PathTextBox.delete("1.0", END)
-        PathTextBox.insert(END, name)
-
-    def docx_reader():
-        docpath = PathTextBox.get('1.0', 'end-1c')
-        text = textract.process(docpath)
-        ResultTextBox.delete('1.0', END)
-        ResultTextBox.insert(END, text)
-
+def readfromimage():
+    path = PathTextBox.get( '1.0', 'end-1c' )
+    if path:
+        im = Image.open(path)
+        text = pytesseract.image_to_string( im, lang='eng' )
+        ResultTextBox.delete( '1.0', END )
+        ResultTextBox.insert( END, text )
         def write():
             # write output to txt file
-            file = open('output.txt', 'w')
-            file.write(ResultTextBox.get('1.0', 'end-1c'))
+            file = open( 'output.txt', 'w' )
+            file.write( ResultTextBox.get( '1.0', 'end-1c' ) )
             file.close()
-
         write()
+    else:
+        ResultTextBox.delete( '1.0', END )
+        ResultTextBox.insert( END, "FILE CANNOT BE READ" )
+def OpenFile():
+    name = askopenfilename( initialdir="/",
+                            filetypes=(("PNG File", "*.png"), ("BMP File", "*.bmp"), ("JPEG File", "*.jpeg"),('PDF File',"*.pdf"),('DOCX File','*.docx')),
+                            title="Choose a file."
+                            )
+    PathTextBox.delete( "1.0", END )
+    PathTextBox.insert( END, name )
+def docx_reader():
+    docpath = PathTextBox.get( '1.0', 'end-1c' )
+    text = textract.process(docpath)
+    ResultTextBox.delete( '1.0', END )
+    ResultTextBox.insert( END, text )
+    def write():
+        # write output to txt file
+        file = open( 'output.txt', 'w' )
+        file.write( ResultTextBox.get( '1.0', 'end-1c' ) )
+        file.close()
+    write()
+def get_pdf_name():
+    pathe = PathTextBox.get( '1.0', 'end-1c' )
+    with pdfplumber.open(pathe) as pdf:
+        page = pdf.pages[0]
+        text = page.extract_text()
+    os.system(f'ocrmypdf {pathe} output.pdf')
+    with pdfplumber.open('output.pdf') as pdf:
+        page = pdf.pages[0]
+        text = page.extract_text(x_tolerance=2)
+    ResultTextBox.delete('1.0', END)
+    ResultTextBox.insert(END, text)
+    def write():
+        file = open( 'output.txt', 'w' )
+        file.write( ResultTextBox.get( '1.0', 'end-1c' ) )
+        file.close()
+    write()
 
-    def get_pdf_name():
-        pathe = PathTextBox.get('1.0', 'end-1c')
-        with pdfplumber.open(pathe) as pdf:
-            page = pdf.pages[0]
-            text = page.extract_text()
-        os.system(f'ocrmypdf {pathe} output.pdf')
-        with pdfplumber.open('output.pdf') as pdf:
-            page = pdf.pages[0]
-            text = page.extract_text(x_tolerance=2)
-        ResultTextBox.delete('1.0', END)
-        ResultTextBox.insert(END, text)
-
-        def write():
-            file = open('output.txt', 'w')
-            file.write(ResultTextBox.get('1.0', 'end-1c'))
-            file.close()
-
-        write()
-x = Extract()
 
 Title = root.title( "Southampton" )
 path = StringVar()
+
+
 # Labels in software.
 HeadLabel = Label(root, text="    Text Extractor " )
 HeadLabel.grid(row=1, column=2,sticky=(W))
@@ -90,16 +82,16 @@ DataLabel = Label( root, text="Text:" )
 DataLabel.grid( row=6, column=1, sticky=(W) )
 
 # Buttons in the software.
-BrowseButton = Button( root, text="Find File Path", command=x.OpenFile )
+BrowseButton = Button( root, text="Find File Path", command=OpenFile )
 BrowseButton.grid( row=2, column=2 )
 
-BrowseButton2 = Button( root, text="Extract Text From Pdf ", command=x.get_pdf_name)
+BrowseButton2 = Button( root, text="Extract Text From Pdf ", command=get_pdf_name)
 BrowseButton2.grid( row=3, column=2 )
 
 
-ReadButton = Button( root, text="Extract Text From Image", command=x.readfromimage)
+ReadButton = Button( root, text="Extract Text From Image", command=readfromimage)
 ReadButton.grid( row=5, column=2 )
-ReadButton2 = Button( root, text="Extract Text From docx", command=x.docx_reader)
+ReadButton2 = Button( root, text="Extract Text From docx", command=docx_reader)
 ReadButton2.grid( row=6, column=2 )
 
 # output textbox
@@ -107,6 +99,7 @@ PathTextBox = Text( root, height=2 )
 PathTextBox.grid( row=4, column=1, columnspan=2 )
 ResultTextBox = Text( root, height=6 )
 ResultTextBox.grid( row=7, column=1, columnspan=2 )
+
 
 
 root.mainloop()
